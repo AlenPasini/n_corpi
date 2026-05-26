f3f1680e6e9677fcb2fea3ed3b4cf92b9d91e69f
GregMontroni <gregoriomontroni@icloud.com>
2026-05-26 23:57:58 +0200

    Add constants and set universe  class

--- n_corpi.cpp
+++ n_corpi.cpp
@@ -3,6 +3,9 @@
 // #ifndef PF_N_CORPI_HPP. // provvisorio
 // #define PF_N_CORPI_HPP
 
+#include <cmath>
+#include <vector>
+
 struct Vector {
   double x;
   double y;
@@ -28,8 +31,10 @@ class Body {
   Vector r_;
   Vector v_;
   Vector a_{0., 0.};
-  Vector a_fut{0., 0.};
+  Vector a_fut_{0., 0.};
   double dt{0.005};
+  double G{6.67 * pow(10., -11.)};
+  double eps { pow(10., -12.) };
 
  public:
   Body(Vector r, Vector v) : r_{r}, v_{v} {}
@@ -43,12 +48,32 @@ class Body {
     r_.y += v_.y * dt + 0.5 * a_.y * dt * dt;
   }
 
-  void a_t() {}
+  void v_t() {
+    v_.x += 0.5 * (a_.x + a_fut_.x) * dt;
+    v_.y += 0.5 * (a_.y + a_fut_.y) * dt;
+  }
 };
 
 class Universe {
-  private:
-  
+ private:
+  std::vector<Body> u_{};
+
+ public:
+  void add(Body const &b) { u_.push_back(b); }
+
+  void a_t(...) {
+    // deve prendere l'accelerazione del Body (Body in input?) e poi fare la
+    // turbo sommatoria. Deve quindi poter accedere anche a tutti gli altri Body
+    // e in particolare alla loro posizione all'interno del mega vector. Deve
+    // infatti saltare lo step della sommatoria del Body in questione.
+  }
+
+  void a_t_complete(...) {
+    // deve fare a_t su tutti i componenti del mega vector.
+    // probabilmente non necessita nessun input...
+    // dovrebbe semplicemente essere un mega ciclo di lunghezza len(u_) che fa n
+    // volte a_t
+  }
 
 }
 // #endif
